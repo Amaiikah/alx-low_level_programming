a38e1d36fe913f19886e7c25f1bdb9915b9eb66f
Amaiikah <okoloanachiamaka@gmail.com>
2023-02-20 10:07:35 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main- prints a string followed by a new line 
+ * main - Entry point
  *
- * Return: 0 when successfull
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
